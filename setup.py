b9616a0d533707275a81188d343a002bacd283b2
Karel Břinda <karel.brinda@gmail.com>
2017-05-10 15:34:36 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
 	#author_email='pypa-dev@googlegroups.com',
 
 	# Choose your license
-	license='GPL',
+	license='GPLv3',
 
 	# See https://pypi.python.org/pypi?%3Aaction=list_classifiers
 	classifiers=[
@@ -55,7 +55,7 @@ setup(
 		'Topic :: Software Development :: Build Tools',
 
 		# Pick your license as you wish (should match "license" above)
-		'License :: OSI Approved :: GNU General Public License (GPL)',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
 		# Specify the Python versions you support here. In particular, ensure
 		# that you indicate whether you support Python 2, Python 3 or both.
